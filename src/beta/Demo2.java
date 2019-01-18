87c08e4aecde5a8856fe57d68ca35ff87cdcaafb
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2019-01-18 14:20:21 -0800

    add Demo2 test class to beta package, add resources directory with log4j2.xml with log4j configs, add log degub, info, error and fatal log tags to both Demo and Demo2 test cases

--- src/beta/Demo2.java
+++ src/beta/Demo2.java
@@ -1,18 +1,18 @@
-package alpha;
+package beta;
 import org.apache.logging.log4j.*;
 
-public class Demo {
-
+public class Demo2 {
+	
 	// Initializes Log object
-	private static Logger log = LogManager.getLogger(Demo.class.getName());
+	private static Logger log = LogManager.getLogger(Demo2.class.getName());
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 		log.debug("I have clicked on button");
 		log.info("Button is displayed");
 		log.error("Button is not displayed");
 		log.fatal("Object is missing");
-		
+
 	}
 
 }
